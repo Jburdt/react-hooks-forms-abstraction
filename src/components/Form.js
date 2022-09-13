3dc3e01ceb3b2a54c702939fba2a56fa747d2ca8
Josh Burd <Jburdt@gmail.com>
2022-09-13 12:28:50 -0400

    done

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,24 +1,64 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
+
+// function Form() {
+//   const [firstName, setFirstName] = useState("John");
+//   const [lastName, setLastName] = useState("Henry");
+
+//   function handleFirstNameChange(event) {
+//     setFirstName(event.target.value);
+//   }
+
+//   function handleLastNameChange(event) {
+//     setLastName(event.target.value);
+//   }
+
+//   return (
+//     <form>
+//       <input type="text" onChange={handleFirstNameChange} value={firstName} />
+//       <input type="text" onChange={handleLastNameChange} value={lastName} />
+//       <button type="submit">Submit</button>
+//     </form>
+//   );
+// }
+
+// export default Form;
+
+import React, {useState} from "react";
 
 function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
+  const [formData, setFormData] = useState({
+    firstName: "John",
+    lastName: "Henry",
+  });
 
   function handleFirstNameChange(event) {
-    setFirstName(event.target.value);
+    setFormData({
+      ...formData,
+      firstName: event.target.value,
+    });
   }
 
   function handleLastNameChange(event) {
-    setLastName(event.target.value);
+    setFormData({
+      ...formData,
+      lastName: event.target.value,
+    });
   }
 
   return (
     <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
-      <button type="submit">Submit</button>
+      <input
+        type="text"
+        onChange={handleFirstNameChange}
+        value={formData.firstName}
+      />
+      <input
+        type="text"
+        onChange={handleLastNameChange}
+        value={formData.lastName}
+      />
     </form>
   );
 }
 
-export default Form;
+export default Form
